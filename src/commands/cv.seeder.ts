c8ac540b1d6f63ed28c2d200f0565d4fa43b00c6
Shams Ben Mefteh <shams.benmefteh@insat.ucar.tn>
2024-11-25 22:09:37 +0100

    added seed

--- src/commands/cv.seeder.ts
+++ src/commands/cv.seeder.ts
@@ -3,7 +3,7 @@ import { AppModule } from "../app.module";
 import { CvService } from '../cv/cv.service';
 import { SkillService } from '../skill/skill.service';
 import { UserService } from '../user/user.service';
-import { randFullName, randEmail, randJobTitle, randFirstName, randNumber, randFilePath } from '@ngneat/falso';
+import { randFullName, randEmail, randJobTitle, randFirstName, randNumber, randFilePath, randUserName, rand, randPassword, randSkill } from '@ngneat/falso';
 
 
 async function bootstrap() {
@@ -24,8 +24,25 @@ async function bootstrap() {
         await cvService.create(cv); // Assume `create` method exists in CvService
       }
 
-    //const userService = app.get(UserService);
-    //const skillService = app.get(SkillService);
+    const userService = app.get(UserService);
+    const users = Array(10).fill(null).map( () => ({
+        username: randUserName(),
+        email: randEmail(),
+        password: randPassword()
+    }))
+
+    for (const user of users) {
+        await userService.create(user); 
+      }
+    
+    const skillService = app.get(SkillService);
+    const skills = Array(10).fill(null).map( () => ({
+        designation: randSkill(),
+    }))
+
+    for (const skill of skills) {
+        await skillService.create(skill); 
+      }
 
     console.log('Database seeded successfully!');
     await app.close();
